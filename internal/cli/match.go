549465f4b5cbb104d6921f6fbc2a8f0a74a08b5f
Arthur Broudoux <arthur.broudoux@ouest-france.fr>
2025-10-28 15:03:14 +0100

    feat: Create CLI

---
+++ internal/cli/match.go
@@ -0,0 +1,58 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/abroudoux/twinpick/internal/match"
+	"github.com/abroudoux/twinpick/internal/scrapper"
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
+
+var (
+	usernames string
+	genres    string
+	platform  string
+)
+
+func init() {
+	matchCmd := &cobra.Command{
+		Use:   "match",
+		Short: "Find a movie based on Users Letterboxd Watchlists",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if usernames == "" {
+				return fmt.Errorf("the param --usernames is required")
+			}
+
+			uList := strings.Split(usernames, ",")
+			gList := []string{}
+			if genres != "" {
+				gList = strings.Split(genres, ",")
+			}
+
+			sParams := scrapper.NewScrapperParams(uList, gList, platform)
+			watchlists := scrapper.ScrapUsersWachtlists(sParams)
+
+			commonFilms, err := match.GetCommonFilms(watchlists)
+			if err != nil {
+				return err
+			}
+
+			if len(commonFilms) == 0 {
+				log.Info("Any common films found 😢")
+				return nil
+			}
+
+			selectedFilm, _ := match.SelectRandomFilm(commonFilms)
+			log.Infof("🎬 Selected film : %s", selectedFilm)
+			return nil
+		},
+	}
+
+	matchCmd.Flags().StringVar(&usernames, "usernames", "", "Usernames Letterboxd split by comma (required)")
+	matchCmd.Flags().StringVar(&genres, "genres", "", "Genres (optional, split by comma)")
+	matchCmd.Flags().StringVar(&platform, "platform", "", "Platform (optional, ex: netflix-fr)")
+
+	rootCmd.AddCommand(matchCmd)
+}
---
+++ internal/cli/root.go
@@ -0,0 +1,18 @@
+package cli
+
+import (
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "twinpick",
+	Short: "Twinpick CLI : find the perfect film based on your Letterboxd Watchlists",
+	Long:  "Twinpick is a tool to help you find the perfect film to watch based on your Letterboxd Watchlists.",
+}
+
+func Run() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
